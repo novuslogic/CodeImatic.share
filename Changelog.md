7fdb61cce359a0a6cc4b81729e9292a2b6685b0c
Adam Craig Johnston <adam@novuslogic.com>
2023-06-18 21:46:15 +1000

    Start of new  ProjectTemplate class

--- Changelog.md
+++ Changelog.md
@@ -1,5 +1,10 @@
 # CodeImatic.share Changelog
 
+18/06/2023
+
+* Start of new  ProjectTemplate class
+
+
 18/04/2023
 
 * Now supports Delphi 10.4 and Delphi 11 NovuscodeLibrary v0.1.0
---
+++ source/ProjectTemplate.pas
@@ -0,0 +1,27 @@
+unit ProjectTemplate;
+
+interface
+
+Uses NovusTemplate2;
+
+type
+  tProjectTemplate = class(tNovusTemplate2)
+  protected
+  private
+    fsTemplateName: String;
+  public
+    class function CreateProjectTemplate: tProjectTemplate;
+  end;
+
+implementation
+
+class function TProjectTemplate.CreateProjectTemplate: tProjectTemplate;
+begin
+  Result :=  tProjectTemplate.Create;
+  Result.StartToken := '<';
+  Result.EndToken := '>';
+  Result.SecondToken := '%';
+  Result.SwapTagNameBlankValue := true;
+end;
+
+end.
